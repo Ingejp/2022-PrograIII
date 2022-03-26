d917eaa6d2d8c859e7e3d498de5a279c8760ccae
JP Personal-PC <ingejp@outlook.com>
2022-03-26 00:55:24 -0600

    Se agregan más validaciones de errores al formulario

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -33,15 +33,16 @@
         <br>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Nombre</label>
-            <input type="text" class="form-control" name="name" id="exampleFormControlInput1" placeholder="Nombre">
+            <input type="text" class="form-control" value="{{old('name')}}" name="name" id="exampleFormControlInput1" placeholder="Nombre">
+            {{--{{$errors->first('name')}}--}}
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Dirección</label>
-            <input type="text" class="form-control" name="address" id="exampleFormControlInput1" placeholder="Dirección">
+            <input type="text" class="form-control" value="{{old('address')}}" name="address" id="exampleFormControlInput1" placeholder="Dirección">
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Teléfono</label>
-            <input type="text" class="form-control" name="phone_number" id="exampleFormControlInput1" placeholder="Teléfono">
+            <input type="text" class="form-control" value="{{old('phone_number')}}" name="phone_number" id="exampleFormControlInput1" placeholder="Teléfono">
         </div>
         <div class="mb-3">
                 <div class="form-group">
@@ -52,7 +53,6 @@
                         @foreach($categories as $category)
                             <option value="{{$category->id}}"> {{$category->description}}  </option>
                         @endforeach
-
                     </select>
             </div>
         </div>
